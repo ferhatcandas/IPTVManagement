fa8fa1b32014a385b5ce38d2202d959538c1a1dd
Ferhat Candaş <candasferhat61@gmail.com>
2020-12-13 19:39:04 +0300

    inital commıt

---
+++ api/IPTVManagement/IPTV.WebApi/Controller/ChannelsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IPTV.WebApi.Controller
+{
+    [Route("channels")]
+    [ApiController]
+    public class ChannelsController : ControllerBase
+    {
+        private readonly Manager manager;
+
+        public ChannelsController(Manager manager)
+        {
+            this.manager = manager;
+        }
+        [HttpGet()]
+        public async Task<IActionResult> GetChannels()
+        {
+            return Ok(manager.GetTVChannels().Select(x => new
+            {
+                Id = x.Id,
+                ChannelName = x.ShowChannelName,
+                IsActive = x.IsActive,
+                Logo = x.Logo,
+                Language = x.Language,
+                Category = x.Category,
+                Country = x.Country,
+                StreamUrl = x.StreamLink,
+                IsFound = x.Found
+            }
+            ).ToList());
+        }
+        [HttpPost()]
+        public async Task<IActionResult> AddChannel()
+        {
+            return Ok();
+        }
+        [HttpPut()]
+        public async Task<IActionResult> UpdateChannel()
+        {
+            return Ok();
+        }
+        [HttpGet("/fetch")]
+        public async Task<IActionResult> FetchLink()
+        {
+            return Ok();
+        }
+
+    }
+}
---
+++ api/IPTVManagement/IPTV.WebApi/Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace IPTV.WebApi
+{
+    public class Startup
+    {
+      public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddOptions();
+            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+            RegisterServices(services);
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            Manager manager = new Manager(new M3UManager(new System.Net.Http.HttpClient(), new DataLayer.FetchRepository()), new TVChannelManager(new DataLayer.TVChannelRepository()));
+
+            services.AddSingleton<Manager>(manager);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseCors();
+            app.UseAuthorization();
+            app.UseCors("CorsPolicy");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
